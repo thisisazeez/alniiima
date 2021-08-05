85e329bb8f69b8e222181339678955e8e1290e06
thisisazeez <imfostersyndrome@gmail.com>
2021-08-05 19:36:15 +0800

    disabled collect static

--- alniima/settings.py
+++ alniima/settings.py
@@ -125,8 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, ''),)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
